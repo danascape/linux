5f1afd679dca39fa84c5b8302866a510430f3979
Stephan Gerhold <stephan@gerhold.net>
2022-11-10 19:13:37 +0100

    drm/panel: longcheer-truly-nt35695: Force DSI video mode
    Command mode is somewhat broken in the mainline kernel.
    Change some magic bits to force video mode for now.
    
    Signed-off-by: Stephan Gerhold <stephan@gerhold.net>

--- drivers/gpu/drm/panel/msm8916-generated/panel-longcheer-truly-nt35695.c
+++ drivers/gpu/drm/panel/msm8916-generated/panel-longcheer-truly-nt35695.c
@@ -565,7 +565,7 @@ static int truly_nt35695_on(struct truly_nt35695 *ctx)
 	mipi_dsi_dcs_write_seq(dsi, 0xff, 0x10);
 	usleep_range(1000, 2000);
 	mipi_dsi_dcs_write_seq(dsi, 0x3b, 0x03, 0x08, 0x02, 0x04, 0x04);
-	mipi_dsi_dcs_write_seq(dsi, 0xbb, 0x10);
+	mipi_dsi_dcs_write_seq(dsi, 0xbb, 0x03); /* 0x10 -> 0x03 for video mode */
 	mipi_dsi_dcs_write_seq(dsi, 0xfb, 0x01);
 	mipi_dsi_dcs_write_seq(dsi, 0x51, 0xff);
 	mipi_dsi_dcs_write_seq(dsi, 0x53, 0x2c);
@@ -731,7 +731,7 @@ static int truly_nt35695_probe(struct mipi_dsi_device *dsi)
 
 	dsi->lanes = 4;
 	dsi->format = MIPI_DSI_FMT_RGB888;
-	dsi->mode_flags = MIPI_DSI_MODE_VIDEO_SYNC_PULSE |
+	dsi->mode_flags = MIPI_DSI_MODE_VIDEO | MIPI_DSI_MODE_VIDEO_SYNC_PULSE |
 			  MIPI_DSI_MODE_NO_EOT_PACKET |
 			  MIPI_DSI_CLOCK_NON_CONTINUOUS | MIPI_DSI_MODE_LPM;
 
