3627047568be0f44a1a76714a42db97fa336d360
Otto Pflüger <otto.pflueger@abscue.de>
2023-04-20 18:45:03 +0200

    ASoC: qdsp6: q6voice-dai: Add VoiceMMode1 DAI
    VoiceMMode1 is used instead of CS-Voice on newer firmwares.
    
    Signed-off-by: Otto Pflüger <otto.pflueger@abscue.de>

--- include/dt-bindings/sound/qcom,q6voice.h
+++ include/dt-bindings/sound/qcom,q6voice.h
@@ -3,5 +3,12 @@
 #define __DT_BINDINGS_Q6_VOICE_H__
 
 #define	CS_VOICE        0
+#define	VOIP            1
+#define	VOLTE           1
+#define	VOICE2          1
+#define	QCHAT           1
+#define	VOWLAN          1
+#define	VOICEMMODE1     1
+#define	VOICEMMODE2     2
 
 #endif /* __DT_BINDINGS_Q6_VOICE_H__ */
